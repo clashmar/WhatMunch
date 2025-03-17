a79a89ef266bf30f9550ce472b170926dae44d70
Charlie Lashmar <charlie@lashmar.com>
2025-03-17 14:24:37 +0000

    Simplify RegisterView using generics.CreateAPIView

--- WhatMunch_Django/WhatMunch_Django/WhatMunch_Django/urls.py
+++ WhatMunch_Django/WhatMunch_Django/WhatMunch_Django/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/auth/', include('myproject.authentication.urls')),
+    path('api/auth/', include('authentication.urls')),
 ]
--- WhatMunch_Django/WhatMunch_Django/authentication/views.py
+++ WhatMunch_Django/WhatMunch_Django/authentication/views.py
@@ -1,24 +1,14 @@
-from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from django.contrib.auth.models import User
-from rest_framework_simplejwt.tokens import RefreshToken
 from .serializers import UserSerializer
+from rest_framework import generics
 
-class RegisterView(APIView):
-    permission_classes = [AllowAny]
-
-    def post(self, request):
-        serializer = UserSerializer(data=request.data)
-        if serializer.is_valid():
-            user = serializer.save()
-            refresh = RefreshToken.for_user(user)
-            return Response({
-                'refresh': str(refresh),
-                'access': str(refresh.access_token),
-            }, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny] 
 
 class TestProtectedView(APIView):
     permission_classes = [IsAuthenticated]
