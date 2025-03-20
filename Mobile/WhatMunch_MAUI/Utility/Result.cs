3e839cc468ae0431b69530e68189e3bc76c634fb
Charlie Lashmar <charlie@lashmar.com>
2025-03-20 15:48:23 +0000

    Create Result UtilityClass

---
+++ Mobile/WhatMunch_MAUI/Utility/Result.cs
@@ -0,0 +1,24 @@
+﻿namespace WhatMunch_MAUI.Utility
+{
+    public class Result<T>
+    {
+        public bool IsSuccess { get; }
+        public T? Value { get; }
+        public string? ErrorMessage { get; }
+
+        private Result(T value)
+        {
+            IsSuccess = true;
+            Value = value;
+        }
+
+        private Result(string errorMessage)
+        {
+            IsSuccess = false;
+            ErrorMessage = errorMessage;
+        }
+
+        public static Result<T> Success(T value) => new(value);
+        public static Result<T> Failure(string errorMessage) => new(errorMessage);
+    }
+}
