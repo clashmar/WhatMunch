49735f7b4e27ab3130cb19e33065fb2edee0d81c
Charlie Lashmar <charlie@lashmar.com>
2025-04-16 19:10:57 +0100

    Create constants for page names

--- Mobile/WhatMunch_MAUI/Converters/FavouriteIconConverter.cs
+++ Mobile/WhatMunch_MAUI/Converters/FavouriteIconConverter.cs
@@ -1,5 +1,7 @@
 ﻿using System.Globalization;
+using WhatMunch_MAUI.Data;
 using WhatMunch_MAUI.Models.Dtos;
+using WhatMunch_MAUI.Views;
 
 namespace WhatMunch_MAUI.Converters
 {
@@ -8,14 +10,14 @@ namespace WhatMunch_MAUI.Converters
         public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
         {
             var place = values[0] as PlaceDto;
-            var mode = values[1] as string;
+            var page = values[1] as string;
 
-            if (place is not null && !string.IsNullOrEmpty(mode))
+            if (place is not null && !string.IsNullOrEmpty(page))
             {
-                return mode switch
+                return page switch
                 {
-                    "saved" => "delete",
-                    "search" => place.IsFavourite ? "liked" : "like",
+                    Constants.SAVED_PLACES_PAGE => "delete",
+                    Constants.SEARCH_RESULTS_PAGE => place.IsFavourite ? "liked" : "like",
                     _ => "liked"
                 };
             }
