c865e55f6f8d38493fd018b7c2fb85d5f61f4de6
Charlie Lashmar <charlie@lashmar.com>
2025-04-25 15:31:42 +0100

    Implement default throttling for backend

--- Backend/WhatMunch_Django/WhatMunch_Django/settings.py
+++ Backend/WhatMunch_Django/WhatMunch_Django/settings.py
@@ -26,6 +26,14 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
     ],
+    "DEFAULT_THROTTLE_CLASSES": [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    "DEFAULT_THROTTLE_RATES": {
+        'anon': '5/m',
+        'user': '10/m'
+    }
 }
 
 SIMPLE_JWT = {
